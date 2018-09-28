8a03e9568277cdae4f9df6c6075ff235faf107d4
Sam Smith <40845516+S1809428@users.noreply.github.com>
2018-09-29 00:55:11 +0630

    29th 1am

---
+++ VacancyLinks/VacancyLinks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VacancyLinks.Startup))]
+namespace VacancyLinks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
